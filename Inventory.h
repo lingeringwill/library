7ebf273ea53214758b3e2bfb0779903eef389e48
joshua ijaola <joshuaijaola@outlook.com>
2021-02-07 00:54:47 -0500

    init

--- Inventory.h
+++ Inventory.h
@@ -7,5 +7,10 @@ class Inventory
  public:
    std::vector <Book> Books;
 
+  void AddBook (Book book);
+  void RemoveBook (Book book);
+  Book FindBookByTitle(std::string title);
+  void CheckOutBook(Book book);
+  void CheckInBook(Book book);
   
 };
--- library.cpp
+++ library.cpp
@@ -5,7 +5,19 @@
 int main()
 {
 
-Book mybook(1, "Harry Potter", "J.K.R");
-std::cout << mybook.Title << std::endl;
+while(true)
+{
+
+    std::cout << "choose an option: " << std::endl;
+    std::cout << "1. add book" << std::endl;
+    std::cout << "2. list all books " << std::endl;
+    std::cout << "3. Check out book " << std::endl;
+    std::cout << "4. Check in book " << std::endl;
+
+    std::cout << "0. Exit";
+
+    int input;
+    std::cin >> input;
+}
 
 }
